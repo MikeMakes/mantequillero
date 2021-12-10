5e2ada470789ad8c31fe7a9da22673a6614382aa
MikeMakes <mikemakes@protonmail.com>
2021-12-10 15:45:40 +0100

    idk

--- js/server.js
+++ js/server.js
@@ -1,37 +1,36 @@
-const { exec } = require("child_process");
+const { spawn } = require("child_process");
     
-    var express=require('express');
-    var app=express();
-    var serverp5=app.listen(3000);
-    app.use(express.static('testp5'));
-    console.log("server running")
-    
-    //var socket=require('socket.io');
-    var socket=require('socket.io', { rememberTransport: false, transports: ['WebSocket', 'Flash Socket', 'AJAX long-polling'] });
-    var io=socket(serverp5);
-    io.sockets.on('connection', newConnection);
-    function newConnection(socket){
-        console.log('new connection: ' + socket.id);
+var express=require('express');
+var app=express();
+var serverp5=app.listen(3000);
+app.use(express.static('testp5'));
+console.log("server running")
 
-        socket.on('angle', recvAngle);
-        function recvAngle(angle){
-            console.log("Recibido angle: "+angle);
+//var socket=require('socket.io');
+var socket=require('socket.io', { rememberTransport: false, transports: ['WebSocket', 'Flash Socket', 'AJAX long-polling'] });
+var io=socket(serverp5);
+io.sockets.on('connection', newConnection);
+function newConnection(socket){
+    console.log('new connection: ' + socket.id);
 
-            exec("ls -la", (error, stdout, stderr) => {
-                if (error) {
-                    console.log(`error: ${error.message}`);
-                    return;
-                }
-                if (stderr) {
-                    console.log(`stderr: ${stderr}`);
-                    return;
-                }
-                console.log(`stdout: ${stdout}`);
-            });
-        }
+    socket.on('angle', recvAngle);
+    function recvAngle(angle){
+        console.log("Recibido angle: "+angle);
 
-        socket.on('module', recvModule);
-        function recvModule(module){
-            console.log("Recibido module: "+module);
+        if (angle < 3.14/2){
+            console.log("+++ ALANTE +++");
+            spawn("ls", ["-la"]);
+        } else if((angle > 3.14/2) && (angle < 3.14)){
+            console.log("--- ATRAS ---");
+            spawn("ls", ["-la"]);
+        }else if(angle > 3.14){
+            console.log("... parao ...");
+            spawn("ls", ["-la"]);
         }
     }
+
+    socket.on('module', recvModule);
+    function recvModule(module){
+        console.log("Recibido module: "+module);
+    }
+}
