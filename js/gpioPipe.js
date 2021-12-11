91f658a97c4b4d826a01a199b19c27bb093ec48d
MikeMakes <mikemakes@protonmail.com>
2021-12-11 19:36:39 +0100

    pipe test

---
+++ js/gpioPipe.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+
+// open in both read & write mode
+// isn't blocked for other process to open the pipe
+const fd = fs.openSync('/dev/pigpio', 'w') 
+
+const input="servo 14 1700";
+console.log('sending:', input)
+fs.writeSync(fd, input)
