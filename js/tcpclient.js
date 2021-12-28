82ba99a9f94d95a17a08853699a288cde668b945
MikeMakes <mikemakes@protonmail.com>
2021-12-28 22:19:21 +0100

    servo=8 cmd, p3 =0 bytes p2=nºgpio

--- js/tcpclient.js
+++ js/tcpclient.js
@@ -7,8 +7,7 @@ var client = new net.Socket();
 client.connect(PORT, HOST, function() {
   console.log('CONNECTED TO: ' + HOST + ':' + PORT);
   // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
-  client.write('servo 14');
-  client.write('1200');
+  client.write('8 14 0');
  
  //client.write('servo');
  //client.write('14');
