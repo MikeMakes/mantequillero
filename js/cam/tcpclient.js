af2d1470b9e5561e08f177943ff9e90914187605
MikeMakes <mikemakes@protonmail.com>
2022-02-27 22:13:44 +0100

    clean code&comments

--- js/cam/tcpclient.js
+++ js/cam/tcpclient.js
@@ -1,11 +1,7 @@
 fs = require('fs');
-let i=0;
-let j=0;
+let frameCounter=0;
 let c=0;
 let out = new Uint8Array(); // new in ES2017
-let startFrame = new Uint8Array();
-let endFrame = new Uint8Array();
-let newFrame = false;
 
 var net = require('net');
 
@@ -15,115 +11,44 @@ var PORT = 3001;
 var client = new net.Socket();
 client.connect(PORT, HOST, function() {
   console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-  // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
-  //client.write('8 14 1200 0 0 0 0');
-  //  client.write('0x8','hex'); 
-  // client.write('0x14','hex');
-  // client.write('1200');
-  // client.write('0');
-  //client.write('0');
-  //client.write('0');
-  // client.write('0');
-  // client.write('0');
 });
 
 // Add a 'data' event handler for the client socket
 // data is what the server sent to this socket
 client.on('data', function(data) {
   console.log("Data event n:  " + c);
-  console.log('Frame: ' + j);
-  /*
-  fs.writeFile("base64_"+i+".jpeg", data, 'base64', function (err) {
-    if (err) return console.log(err);
-    console.log('END BASE64');
-  });//, [encoding], [callback])
-  fs.writeFile("ascii_"+i+".jpeg", data, 'ascii', function (err) {
-    if (err) return console.log(err);
-    console.log('END ascii');
-  });//, [encoding], [callback])
+  console.log('Frame: ' + frameCounter);
 
-  fs.writeFile("utf8_"+i+".jpeg", data, 'utf8', function (err) {
-    if (err) return console.log(err);
-    console.log('END utf8');
-  });//, [encoding], [callback])
-  */
-
-  //Slice by jpeg FF D8 start
+  //Slice by knowing all jpeg start with 0xFF 0xD8 bytes
   let jpegstart = data.findIndex((element,index,array) => { 
-    //console.log("searching");
-    if(element===0xFF) console.log("0xFF at index:  " + index);
-    if(array[index+1]===0xD8) console.log("0xD8 at index:  " + index+1);
+    //if(element===0xFF) console.log("0xFF at index:  " + index);
+    //if(array[index+1]===0xD8) console.log("0xD8 at index:  " + index+1);
     if((element===0xFF)&&(array[index+1]===0xD8)){
-      console.log("start at: " + index);
+      //console.log("Start of frame at: " + index);
       return true;
     }  
   });
-  /*
-  if(jpegstart>0){
-    endFrame = data.slice(0,jpegstart-1);
-    startFrame = data.slice(jpegstart);
-    out = Buffer.concat([out,endFrame]);
-    newFrame=true;
-    console.log("New Frame");
-  }else{
-    newFrame=false;
-    out = Buffer.concat([out,data]);
-  }*/
 
-  if(jpegstart>=0){
-    newFrame=true;
+  //Save frame
+  if((jpegstart>=0)&&out.length>0){
     console.log("New Frame");
+    frameCounter++;
 
-    //endFrame = data.slice(0,jpegstart-1);
-    //startFrame = data.slice(jpegstart);
-    fs.writeFile("final_"+j+".jpeg", out, 'base64', function (err) {
+    fs.writeFile("frame_"+frameCounter+".jpeg", out, 'base64', function (err) {
       if (err) return console.log(err);
-      else console.log("writed final_"+j+".jpeg");
+      else console.log("writed final_"+frameCounter+".jpeg");
     });//, [encoding], [callback])
 
     out =new Uint8Array();
-  }else{
-    newFrame=false;
   }
 
   out = Buffer.concat([out,data]);
-  
-  /*
-  if(i<3) {
-    out = Buffer.concat([out,data]);
-  }
-
-  if(i===2) {
-    i=-1;
-
-    fs.writeFile("final_"+j+".jpeg", out, 'base64', function (err) {
-      if (err) return console.log(err);
-      else console.log("writed final_"+j+".jpeg");
-    });//, [encoding], [callback])
-
-    out=new Uint8Array();
-
-    j++;
-    //out=[...startFrame]; 
-  }
-  */
-
-  if(newFrame){
-    //out =new Uint8Array();
-    //out = Buffer.concat([out,startFrame]);
-
-    j++;
-
-  }
-
 
   // Close the client socket completely
-  if(j===100) client.destroy();
-  //client.destroy();
-  //i++;
+  if(frameCounter===100) client.destroy();
   c++;
 
-  console.log(out.length);
+  //console.log(out.length);
 });
 
 // Add a 'close' event handler for the client socket
