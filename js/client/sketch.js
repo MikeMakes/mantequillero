73a399c715a53d414b8f8b8130eff9c560b2a1e3
MikeMakes <mikemakes@protonmail.com>
2022-03-03 00:26:51 +0100

    dummy pwm when server disconnected

--- js/client/sketch.js
+++ js/client/sketch.js
@@ -12,10 +12,19 @@ let angleLast=0.0;
 let vel_data = new Array(2);
 let pwm_data = [0,0];
 
-var socket;
+//pwm constant for dummy kinematics
+const SERVO_CCW_MIN=700;
+const SERVO_MID=1500;
+const SERVO_CW_MAX=2300;
+const SERVO_DEAD_BAND=90;
+const VEL_LIN_MAX=0.33*100;
+
+//image data
 let imgbuff=new Uint8Array();
-let img;
+let img; //dom element ref
 let newFrame=false;
+
+var socket;
 
 function _arrayBufferToBase64( buffer ) {
     var binary = '';
@@ -31,13 +40,10 @@ function setup() {
 	socket = io.connect(window.location.origin);
 	//socket = io.connect(window.location.origin,{ rememberTransport: false, transports: ['WebSocket', 'Flash Socket', 'AJAX long-polling']});
 
-	//socket.on('pwm', pwm_visualizer);
-	//function pwm_visualizer(pwm){
 	socket.on("pwm_data", (pwm) => {
 		pwm_data = pwm;
 		console.log('pwm_data from server: '+pwm_data);
 	});
-	//}
 
 	socket.on("img_data", (imgbuffer) => {
 		imgbuff=imgbuffer;
@@ -46,6 +52,8 @@ function setup() {
 
 	img = createImg("data:image/jpeg;base64," + '','jpeg image captured');
 	img.style("z-index","-1");
+	img.position(0, 0);
+	img.size(windowWidth, windowHeight);
 
 	createCanvas(windowWidth, windowHeight);
 	frameRate(24);
@@ -55,16 +63,15 @@ function setup() {
 
 function draw() {
 	clear();
-	//background(100, 100, 100, 0); //in future bkg will be webcam capture
 	background(255,255,255,0);
 
 	if(newFrame && imgbuff.length>0){
 		var b64 = _arrayBufferToBase64(imgbuff);
 		img.attribute('src', "data:image/jpeg;base64," + b64);
 		newFrame=false;
+	}else{
+		if(socket.disconnected) img.attribute('src', "resources/nocam.jpg");
 	}
-	img.position(centerX-640/2, centerY-480/2);
-	img.size(640,480);
 
 	//coordinate system
 	line(centerX,centerY,mouseX,mouseY);
@@ -86,7 +93,6 @@ function draw() {
 
 	//get velocity mouse input
 	if(mouseIsPressed){
-		//console.log("mouseIsPressed");
 		modd=sqrt(pow(mx,2)+pow(my,2));
 		if(my<0) modd=-modd;
 		angle=atan2(my,mx);
@@ -95,29 +101,25 @@ function draw() {
 
 		reduce_lin=false;
 		reduce_ang=false;
-		//send new velocity command
-		//if(modd<(windowHeight-100)/2){
-		//	send_vel();
-		//}
 	}
 
 	//keyboard velocity input, hold increase, release stops 
-	if (keyIsDown(65)) {
+	if (keyIsDown(65)) {//right
 		angle -= 0.2;
 		if(angle<=-Math.PI) angle=-Math.PI; 
 		reduce_ang=false;
 	}
-	if (keyIsDown(68)) {
+	if (keyIsDown(68)) {//left
 		angle += 0.2;
 		if(angle>=Math.PI) angle=Math.PI;
 		reduce_ang=false;
 	}
-	if (keyIsDown(83)) {
+	if (keyIsDown(83)) {//down
 		modd -= 20;
 		if(modd<-500) modd=-500;
 		reduce_lin=false;
 	}
-	if (keyIsDown(87)) {
+	if (keyIsDown(87)) {//up
 		modd += 20;
 		if(modd>500) modd=500;
 		reduce_lin=false;
@@ -154,74 +156,16 @@ function send_vel(){
 
 	socket.emit('vel_data',vel_data);
 	console.log("Emit vel_data: "+ vel_data);
-	if(socket.disconnected) pwm_data=[...kinetic(vel_data[0],vel_data[1])]; //offline fake kinematics
+	//if(socket.disconnected) pwm_data=[...kinetic(vel_data[0],vel_data[1])]; //offline fake kinematics
+	if(socket.disconnected) pwm_data=[...apply_pwm(kinetic(vel_data[0],vel_data[1]))]; //offline fake pwm
+	
+	/* //debug print speed on screen
 	textSize(32);
 	fill(0, 255, 0);
 	text(vel_data[0], centerX, centerY);
 	text(vel_data[1], centerX, centerY+50);
+	*/
 }
-
-function keyPressed(){
-	console.log("keycode "+keyCode);
-
-}
-
-/*
-function keyTyped() {
-	let sendmsg = true;
-	if (keyCode === 'a'){//left
-		//if(angle>=-Math.PI+0.1)
-		angle=angle-0.1;		
-	} else if (keyCode === 'd'){//RIGHT_ARROW){
-		if(angle<=Math.PI-0.1) angle=angle+0.1;
-	} else if (keyCode === 's'){//down
-		if(modd>=-999) modd=modd-1;
-	} else if (keyCode === 'w'){//up
-		if(modd<=999) modd=modd+1;
-	} else{
-		console.log("!msg");
-		sendmsg=false;
-	}
-
-	console.log("keypressed"+keyCode);
-
-	if (sendmsg){
-		console.log("key send");
-		send_vel();
-	}
-}
-*/
-/*
-let k=false;
-function keyPressed() {
-	k=true;
-	let sendmsg = true;
-	if (keyCode === 65){//left
-		//if(angle>=-Math.PI+0.1)
-		angle=angle-0.1;		
-	} else if (keyCode === 68){//RIGHT_ARROW){
-		if(angle<=Math.PI-0.1) angle=angle+0.1;
-	} else if (keyCode === 83){//down
-		if(modd>=-999) modd=modd-1;
-	} else if (keyCode === 87){//up
-		if(modd<=999) modd=modd+1;
-	} else{
-		console.log("!msg");
-		sendmsg=false;
-	}
-
-	console.log("keypressed"+keyCode);
-
-	if (sendmsg){
-		console.log("key send");
-		send_vel();
-	}
-}
-
-function keyReleased(){
-	k=false;
-}
-*/
 
 function kinetic(_lin,_ang){ //kinematics (lin,ang)=>(left and right servos' speed)
 	let SERVO_VEL=[0,0];
@@ -248,3 +192,33 @@ function kinetic(_lin,_ang){ //kinematics (lin,ang)=>(left and right servos' spe
 
 	return SERVO_VEL;
 }
+
+function apply_pwm(SERVO_VEL){ //(left,right) speed => (left,right) pwm
+	let pwm_values = SERVO_VEL.map(abs_pwm);
+	function abs_pwm(value){
+		let pwm=(SERVO_CW_MAX-SERVO_CCW_MIN)/(2*VEL_LIN_MAX) * value + SERVO_MID;
+
+		//if(pwm>0) pwm = Math.floor(pwm); //round to int
+		//else pwm = Math.ceil(pwm);
+		pwm=Math.round(pwm);
+
+		//console.log("pwm1  "+pwm);
+		if(pwm>SERVO_CW_MAX) return SERVO_CW_MAX; //max limit
+		else if(pwm<SERVO_CCW_MIN) return SERVO_CCW_MIN; //min limit
+		else if(Math.abs(pwm-SERVO_MID)<SERVO_DEAD_BAND*1) return 0; //stop aprox
+		else return pwm;
+	}
+
+	console.log("\n\nDummy pwm write:\n")
+	console.log("\tLEFT: "+pwm_values[0]);
+	console.log("\tRIGHT: "+pwm_values[1]);
+	
+	return pwm_values;
+}
+
+/*
+function keyPressed(){
+	console.log("keycode "+keyCode);
+
+}
+*/
